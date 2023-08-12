b1504a325eae3f9fd16c0948505bf9123c76b6de
SomeBody <141774988+threeco123@users.noreply.github.com>
2023-08-13 02:56:26 +0300

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,7 @@ import { ProductManager } from './product-manager';
 import { Product } from './product';
 
 const productManager = new ProductManager();
+const product = new Product();
 
 const burger = productManager.createProduct(PRODUCT_TYPE.BURGER);
 const pizza = productManager.createProduct(PRODUCT_TYPE.PIZZA);
